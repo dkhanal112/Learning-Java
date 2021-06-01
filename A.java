d9c081f62bf25bec6ec8d8c96946e3959ba6a37e
Dipesh Khanal <dkhanal112@gmail.com>
2021-06-01 09:31:13 +0545

    some more terms added

--- A.java
+++ A.java
@@ -27,6 +27,9 @@ class Test{
 
 // Interface is a mechanism. It helps to achieve multiple inheritance in java also it hepls us to achive extraction in java.
 //  In order to create the interface we must have to write interface keyword. A interface always cosist a method name without a body.
+// The access specifier, it consist only public, abstract and final only used in interface method declaration. 
+// The class which need to implement the interface need to give the working functionality within the method.
+// Interface cannot be instanciated.
 
 interface IAnimal{
   public void color();  
